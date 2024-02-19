86b95ffd3ec6faf41fc315f2baf32a0a69571243
Adrian Onica <adrian.onica@bca.com>
2024-02-19 17:12:10 +0200

    Configured gradle and ktlint and detekt for static code analisys

--- app/src/main/java/com/adi/begrateful/MainActivity.kt
+++ app/src/main/java/com/adi/begrateful/MainActivity.kt
@@ -1,34 +1,9 @@
 package com.adi.begrateful
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.adi.begrateful.ui.theme.BeGratefulTheme
-import com.adi.begrateful.ui.theme.Gray
-import com.adi.begrateful.ui.theme.LightGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,4 +14,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
